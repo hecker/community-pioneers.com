a0e107a89dddb246456db8a082e641a11c18a247
Jan Hecker <23746655+janhecker@users.noreply.github.com>
2023-07-29 23:14:46 +0200

    👋 Initial commit.

---
+++ app/TypeformButton.tsx
@@ -0,0 +1,15 @@
+// https://github.com/Typeform/embed/tree/main/packages/embed-react
+import { PopupButton } from "@typeform/embed-react";
+
+const TypeformButton = () => {
+  return (
+    <PopupButton
+      id="CjjEDWRH"
+      className="px-2.5 py-1 rounded-md bg-green text-white"
+    >
+      Register now
+    </PopupButton>
+  );
+};
+
+export default TypeformButton;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: "#f5f5f5",
+      black: "#0a0a0a",
+      green: "#46C067",
     },
+    extend: {},
   },
   plugins: [],
-}
+};
